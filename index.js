914440a21b91e70962b8457eb871e3ff50803c6d
beso <besogigashvili818@gmail.com>
2022-05-07 16:25:02 +0400

    test files

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('./src/app')
+const pool = require('./src/pool');
+
+pool.connect({
+    host: 'localhost',
+    port: 5432,
+    database: 'socialnetwork',
+    user: 'postgres',
+    password: 'beso123'
+}).then(() => {
+    app().listen(3005 , () => {
+        console.log('Listening on port 3005');
+    })
+}).catch(err => console.log(err))
